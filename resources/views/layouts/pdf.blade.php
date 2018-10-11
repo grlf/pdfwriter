34d5081caa7ab52be2a199786614fe3842fc3abe
John Hoopes <john.z.hoopes@gmail.com>
2018-10-10 22:14:35 -0500

    adding 100% width for body and html

--- resources/views/layouts/pdf.blade.php
+++ resources/views/layouts/pdf.blade.php
@@ -3,6 +3,13 @@
 <head>
     <meta charset="UTF-8">
 
+    <style>
+        html, body {
+            width: 100%;
+        }
+    </style>
+
+
     @if (sizeof(\Config::get('pdfwriter')['pdf_css']) > 0)
         @foreach (\Config::get('pdfwriter')['pdf_css'] as $script)
             <link href="{{ url('/css/' . $script) }}" rel="stylesheet">
